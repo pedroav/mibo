bd9b2dafee6c082405780909182edd347b2156c3
Pedro Viana <pedroav@gmail.com>
2020-03-29 11:51:54 -0300

    Update aws-s3.sh
    remove credential

--- aws-s3.sh
+++ aws-s3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Remove arquivos menores que 1MB
-ps -C aws >/dev/null && echo "`date` - AWS Script is running" >> /var/log/messages || find /media/pi/PE32GB/Videos/IC3/ -type f -size -900k -exec rm -f {} \; 
+ps -C aws >/dev/null && echo "`date` - AWS Script is running" >> /var/log/messages || find PATH-DO-VIDEO -type f -size -900k -exec rm -f {} \; 
 
 #Copia arquivos para AWS S3
-ps -C aws >/dev/null && echo "`date` - AWS Script is running" >> /var/log/messages || /home/pi/.local/lib/aws/bin/aws s3 mv /media/pi/PE32GB/Videos/ s3://cam-mibo --recursive
+ps -C aws >/dev/null && echo "`date` - AWS Script is running" >> /var/log/messages || /home/USER/.local/lib/aws/bin/aws s3 mv PATH-DO-VIDEO s3://FOLDER-S3 --recursive
