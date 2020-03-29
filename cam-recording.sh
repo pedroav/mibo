2992b26f01ed9f868ce2b2192fd5bcda5e5f0114
Pedro Viana <pedroav@gmail.com>
2020-03-29 18:39:36 -0300

    v3

--- cam-recording.sh
+++ cam-recording.sh
@@ -4,4 +4,4 @@ killall ffmpeg
 
 ps -aux |grep -v grep |grep -i $PASS_CAM1 >/dev/null && echo "`date` - FFMPEG IC3 is running" >> /var/log/messages || `ffmpeg -i rtsp://admin:$PASS_CAM1@IC3-Library/cam/realmonitor -acodec copy -vcodec copy -f segment -strftime 1 -segment_time 300 -segment_format mp4 $PATH_VIDEO_LOCAL_CAM1/library-%Y-%m-%d_%H-%M-%S.mp4`&
 
-ps -aux |grep -v grep |grep -i $PASS_CAM2 >/dev/null && echo "`date` - FFMPEG IC5 is running" >> /var/log/messages || `ffmpeg -i rtsp://admin:$PASS_CAM2@IC5-Back/cam/realmonitor -acodec copy -vcodec copy -f segment -strftime 1 -segment_time 300 -segment_format mp4 $PATH_VIDEO_CAM2/backyard-%Y-%m-%d_%H-%M-%S.mp4`&
+ps -aux |grep -v grep |grep -i $PASS_CAM2 >/dev/null && echo "`date` - FFMPEG IC5 is running" >> /var/log/messages || `ffmpeg -i rtsp://admin:$PASS_CAM2@IC5-Back/cam/realmonitor -acodec copy -vcodec copy -f segment -strftime 1 -segment_time 300 -segment_format mp4 $PATH_VIDEO_LOCAL_CAM2/backyard-%Y-%m-%d_%H-%M-%S.mp4`&
