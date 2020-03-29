6535a8282589947f68d38a7b1aa90859e1ae8a28
Pedro Viana <pedroav@gmail.com>
2020-03-29 18:41:44 -0300

    v5

--- aws-s3.sh
+++ aws-s3.sh
@@ -3,4 +3,4 @@
 ps -C aws >/dev/null && echo "`date` - AWS Script is running" >> /var/log/messages || find $PATH_VIDEO_LOCAL_CAM1 -type f -size -900k -exec rm -f {} \; 
 
 #Copiar todos os arquivos para AWS S3
-ps -C aws >/dev/null && echo "`date` - AWS Script is running" >> /var/log/messages || aws s3 sync $PATH_VIDEO_LOCAL $PATH_VIDEO_S3 --recursive
+ps -C aws >/dev/null && echo "`date` - AWS Script is running" >> /var/log/messages || aws s3 sync $PATH_VIDEO_LOCAL $PATH_VIDEO_S3
