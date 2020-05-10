71dc953063c0d5aeb66004ae8e905b8348cc2c6c
pedroav <pedroav@gmail.com>
2020-05-10 17:22:50 -0300

    v7

--- aws-s3.sh
+++ aws-s3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Remover arquivos menores que 900k
-ps -C aws >/dev/null && echo "`date` - AWS Script is running" >> /var/log/messages || find $PATH_VIDEO_LOCAL_CAM1 -type f -size -900k -exec rm -f {} \; 
+ps -C aws >/dev/null && echo "`date` - AWS Script is running" >> /var/log/messages || find $PATH_VIDEO_LOCAL -type f -size -900k -exec rm -f {} \; 
 
 #Copiar todos os arquivos para AWS S3
 ps -C aws >/dev/null && echo "`date` - AWS Script is running" >> /var/log/messages || aws s3 sync $PATH_VIDEO_LOCAL $PATH_VIDEO_S3 --delete
