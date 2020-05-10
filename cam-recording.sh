4047274766884973a094c0f68f7a39cf50372ce7
pedroav <pedroav@gmail.com>
2020-05-10 19:52:04 -0300

    v5

--- cam-recording.sh
+++ cam-recording.sh
@@ -2,6 +2,9 @@
 killall ffmpeg
 
 
-ps -aux |grep -v grep |grep -i $PASS_CAM1 >/dev/null && echo "`date` - FFMPEG IC3 is running" >> /var/log/messages || `ffmpeg -i rtsp://admin:$PASS_CAM1@IC3-Library/cam/realmonitor -acodec copy -vcodec copy -f segment -strftime 1 -segment_time 300 -segment_format mp4 $PATH_VIDEO_LOCAL/%Y-%m-%d_%H-%M-%S-library.mp4`&
 
-ps -aux |grep -v grep |grep -i $PASS_CAM2 >/dev/null && echo "`date` - FFMPEG IC5 is running" >> /var/log/messages || `ffmpeg -i rtsp://admin:$PASS_CAM2@IC5-Back/cam/realmonitor -acodec copy -vcodec copy -f segment -strftime 1 -segment_time 300 -segment_format mp4 $PATH_VIDEO_LOCAL/%Y-%m-%d_%H-%M-%S-backyard.mp4`&
+mkdir -p /mnt/storage/videos/$(date +%Y)/$(date +%m)/$(date +%d)
+
+ps -aux |grep -v grep |grep -i $PASS_CAM1 >/dev/null && echo "`date` - FFMPEG IC3 is running" >> /var/log/messages || `ffmpeg -i rtsp://admin:$PASS_CAM1@IC3-Library/cam/realmonitor -acodec copy -vcodec copy -f segment -strftime 1 -segment_time 300 -segment_format mp4 $PATH_VIDEO_LOCAL/%y/%m/%d/%Y-%m-%d_%H-%M-%S-library.mp4`&
+
+ps -aux |grep -v grep |grep -i $PASS_CAM2 >/dev/null && echo "`date` - FFMPEG IC5 is running" >> /var/log/messages || `ffmpeg -i rtsp://admin:$PASS_CAM2@IC5-Back/cam/realmonitor -acodec copy -vcodec copy -f segment -strftime 1 -segment_time 300 -segment_format mp4 $PATH_VIDEO_LOCAL/%Y/%m/%d/%Y-%m-%d_%H-%M-%S-backyard.mp4`&
