1d9d63ba30c41f56822fa1430cbb93d4fdb55645
Pedro Viana <pedroav@gmail.com>
2020-05-10 21:04:33 -0300

    v7

--- cam-recording.sh
+++ cam-recording.sh
@@ -7,4 +7,4 @@ ps -aux |grep -v grep |grep -i $PASS_CAM1 >/dev/null && echo "`date` - FFMPEG IC
 
 ps -aux |grep -v grep |grep -i $PASS_CAM2 >/dev/null && echo "`date` - FFMPEG IC5 Back is running" >> /var/log/messages || `ffmpeg -i rtsp://admin:$PASS_CAM2@IC5-Back/cam/realmonitor -acodec copy -vcodec copy -f segment -strftime 1 -segment_time 300 -segment_format mp4 $PATH_VIDEO_LOCAL/%Y/%m/%d/%Y-%m-%d_%H-%M-%S-backyard.mp4`&
 
-ps -aux |grep -v grep |grep -i $PASS_CAM2 >/dev/null && echo "`date` - FFMPEG IC5 Front is running" >> /var/log/messages || `ffmpeg -i rtsp://admin:$PASS_CAM3@IC5-Front/cam/realmonitor -acodec copy -vcodec copy -f segment -strftime 1 -segment_time 300 -segment_format mp4 $PATH_VIDEO_LOCAL/%Y/%m/%d/%Y-%m-%d_%H-%M-%S-front.mp4`&
+ps -aux |grep -v grep |grep -i $PASS_CAM3 >/dev/null && echo "`date` - FFMPEG IC5 Front is running" >> /var/log/messages || `ffmpeg -i rtsp://admin:$PASS_CAM3@IC5-Front/cam/realmonitor -acodec copy -vcodec copy -f segment -strftime 1 -segment_time 300 -segment_format mp4 $PATH_VIDEO_LOCAL/%Y/%m/%d/%Y-%m-%d_%H-%M-%S-front.mp4`&
